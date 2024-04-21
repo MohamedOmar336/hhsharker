a5d85c551b3a05c0765e811a8f517feb4a10f01d
mhameed <mhameed@ntgclarity.com>
2024-04-21 13:05:11 +0200

    add layouts-admin and product (model-controller-migration)

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+
+
+    <meta charset="utf-8" />
+    <title>Metrica - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets-admin/images/favicon.ico">
+
+
+
+    <!-- App css -->
+    <link href="assets-admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets-admin/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets-admin/css/app.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body id="body">
+    <!-- leftbar-tab-menu -->
+    @include('admin.layouts.side-nav')
+
+    <!-- end leftbar-tab-menu-->
+
+    <!-- Top Bar Start -->
+    <!-- Top Bar Start -->
+    <!-- Navbar -->
+    @include('admin.layouts.top-nav')
+    <!-- end navbar-->
+    <!-- Top Bar End -->
+    <!-- Top Bar End -->
+
+    <div class="page-wrapper">
+        @yield('content')
+    </div>
+    <!-- end page-wrapper -->
+
+    <!-- Javascript  -->
+    <!-- vendor js -->
+
+    <script src="assets-admin/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets-admin/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets-admin/libs/feather-icons/feather.min.js"></script>
+
+    <script src="assets-admin/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="assets-admin/js/pages/analytics-index.init.js"></script>
+    <!-- App js -->
+    <script src="assets-admin/js/app.js"></script>
+
+</body>
+<!--end body-->
+
+<!-- Mirrored from mannatthemes.com/metrica/default/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 17 Apr 2024 17:45:40 GMT -->
+
+</html>
