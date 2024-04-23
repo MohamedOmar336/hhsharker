f1ec02199dea7d218f4c5cc742485fadd2fa1513
Batool-Anwar <142293345+Batool-Anwar@users.noreply.github.com>
2024-04-23 01:18:45 -0800

    Update scripts.blade.php

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,12 +1,12 @@
 
 <!-- Javascript  -->  
 <!-- vendor js -->
-<script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-<script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets_admin/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets_admin/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets_admin/libs/feather-icons/feather.min.js') }}"></script>
 
-<script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('assets/js/pages/analytics-index.init.js') }}"></script>
+<script src="{{ asset('assets_admin/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets_admin/js/pages/analytics-index.init.js') }}"></script>
 <!-- App js -->
-<script src="{{ asset('assets/js/app.js') }}"></script>
+<script src="{{ asset('assets_admin/js/app.js') }}"></script>
 
