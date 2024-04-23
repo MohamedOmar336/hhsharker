a1f18664ec2eb9472c756e13d5e4cd3cb624ac6a
Batool-Anwar <142293345+Batool-Anwar@users.noreply.github.com>
2024-04-23 00:59:53 -0800

    Add files via upload

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,9 @@
+
+
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+<!-- App css -->
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,1 +1,12 @@
 
+<!-- Javascript  -->  
+<!-- vendor js -->
+<script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+
+<script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/js/pages/analytics-index.init.js') }}"></script>
+<!-- App js -->
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
