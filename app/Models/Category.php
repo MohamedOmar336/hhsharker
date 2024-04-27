1522bd0902ba543ecdb1013585a667acd6cef223
Batool-Anwar <142293345+Batool-Anwar@users.noreply.github.com>
2024-04-26 22:53:53 -0800

    Add files via upload

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'image',
+        'parent_id',
+        'level',
+        'id_path',
+        'slug',
+        'active',
+    ];
 }
