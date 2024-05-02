e60835b2c9fb7ff9f5703896b14ba1b97d7f253e
mhameed <mhameed@ntgclarity.com>
2024-05-02 13:06:34 +0300

    fix file path

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,9 +1,9 @@
 
 
 <!-- App favicon -->
-<link rel="shortcut icon" href="{{ asset('assets_admin/images/favicon.ico') }}">
+<link rel="shortcut icon" href="{{ asset('assets-admin/images/favicon.ico') }}">
 
 <!-- App css -->
-<link href="{{ asset('assets_admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets_admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets_admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets-admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets-admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets-admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,12 +1,12 @@
 
-<!-- Javascript  -->  
+<!-- Javascript  -->
 <!-- vendor js -->
-<script src="{{ asset('assets_admin/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets_admin/libs/simplebar/simplebar.min.js') }}"></script>
-<script src="{{ asset('assets_admin/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets-admin/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets-admin/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets-admin/libs/feather-icons/feather.min.js') }}"></script>
 
-<script src="{{ asset('assets_admin/libs/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('assets_admin/js/pages/analytics-index.init.js') }}"></script>
+<script src="{{ asset('assets-admin/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets-admin/js/pages/analytics-index.init.js') }}"></script>
 <!-- App js -->
-<script src="{{ asset('assets_admin/js/app.js') }}"></script>
+<script src="{{ asset('assets-admin/js/app.js') }}"></script>
 
