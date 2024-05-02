a9325cf2af4437cf84608068ba7460b8ead249ca
Batool Anwar <hardanbatool@gmail.com>
2024-05-02 09:39:46 -0800

    Merge remote changes into new_branch

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,20 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
-<<<<<<< HEAD
-
-class ProductController extends Controller
-{
-    public function index()
-    {
-        $products = Product::all();
-        return view('admin.products.index', compact('products'));
-    }
-
-=======
 use App\Enums\EnumsSettings;
-use App\Models\User;
-use App\Notifications\AccountActivated;
 
 class ProductController extends Controller
 {
@@ -39,17 +26,11 @@ class ProductController extends Controller
      *
      * @return \Illuminate\View\View
      */
->>>>>>> 088d0899d75ec8799ed092a2bc34374d64b41f04
     public function create()
     {
         return view('admin.products.create');
     }
 
-<<<<<<< HEAD
-    public function store(Request $request)
-    {
-        $request->validate([
-=======
     /**
      * Store a newly created product in storage.
      *
@@ -59,52 +40,17 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $data = $request->validate([
->>>>>>> 088d0899d75ec8799ed092a2bc34374d64b41f04
             'name_ar' => 'required|string',
             'name_en' => 'required|string',
             'description_ar' => 'required|string',
             'description_en' => 'required|string',
-<<<<<<< HEAD
-            'slug' => 'required|string',
-            'price' => 'required|numeric',
-            'quantity' => 'required|integer',
-            'is_available' => 'required|boolean',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
-
-        // Handle image upload
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('images'), $imageName);
-        } else {
-            $imageName = 'default.jpg'; // Provide a default image if no image is uploaded
-        }
-
-        Product::create([
-            'name_ar' => $request->name_ar,
-            'name_en' => $request->name_en,
-            'description_ar' => $request->description_ar,
-            'description_en' => $request->description_en,
-            'slug' => $request->slug,
-            'price' => $request->price,
-            'quantity' =>$request->quantity,
-            'is_available' => $request->is_available,
-            'image_url' => $imageName,
-        ]);
-
-        return redirect()->route('products.index')->with('success', 'Product created successfully.');
-    }
-
-=======
             'price' => 'required|numeric',
             'quantity' => 'required|integer',
             'is_available' => 'nullable|boolean',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
-        $imageName = $this->uploadImage($request->file('image'));
-
+        $imageName = uploadImage($request->file('image'));
 
         $data['image_url'] = $imageName;
 
@@ -127,42 +73,12 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\View\View
      */
->>>>>>> 088d0899d75ec8799ed092a2bc34374d64b41f04
     public function edit($id)
     {
         $product = Product::findOrFail($id);
         return view('admin.products.edit', compact('product'));
     }
 
-<<<<<<< HEAD
-    public function update(Request $request, Product $product)
-    {
-        
-        $product->name_ar = $request->name_ar;
-        $product->name_en = $request->name_en;
-        $product->description_ar = $request->description_ar;
-        $product->description_en = $request->description_en;
-        $product->slug = $request->slug;
-        $product->price = $request->price;
-        $product->quantity = $request->quantity;
-        $product->is_available = $request->is_available;
-    
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('images'), $imageName);
-            $product->image_url = $imageName;
-        } else {
-            // Keep the current image if no new image is uploaded
-            $imageName = $product->image_url;
-        }
-        $product->save();
-
-        return redirect()->route('products.index')->with('success', 'Product updated successfully.');
-    }
-   
-
-=======
     /**
      * Update the specified product in storage.
      *
@@ -188,7 +104,7 @@ class ProductController extends Controller
         $record->fill($validatedData);
 
         if ($request->hasFile('image')) {
-            $imageName = $this->uploadImage($request->file('image'));
+            $imageName = uploadImage($request->file('image'));
             $record->image_url = $imageName;
         }
 
@@ -209,28 +125,12 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\RedirectResponse
      */
->>>>>>> 088d0899d75ec8799ed092a2bc34374d64b41f04
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
         $product->delete();
 
         return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
-<<<<<<< HEAD
-=======
     }
 
-    /**
-     * Uploads the image and returns the image name.
-     *
-     * @param  \Illuminate\Http\UploadedFile  $image
-     * @return string
-     */
-    private function uploadImage($image)
-    {
-        $imageName = time() . '.' . $image->getClientOriginalExtension();
-        $image->move(public_path('images'), $imageName);
-        return $imageName;
->>>>>>> 088d0899d75ec8799ed092a2bc34374d64b41f04
-    }
 }
---
+++ resources/views/admin/Categories/create.blade.php
@@ -0,0 +1,101 @@
+@extends('admin.layouts.app')
+
+@section('content')       
+
+            <!-- Page Content-->
+            <div class="page-content-tab">
+
+<div class="container-fluid">
+    <!-- Page-Title -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <div class="float-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ url('/home') }}">Metrica</a>
+                        </li><!--end nav-item-->
+                        <li class="breadcrumb-item"><a href="{{ url('/categories') }}">Categories</a>
+                        </li><!--end nav-item-->
+                        <li class="breadcrumb-item active">Add</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">Add Category</h4>
+            </div><!--end page-title-box-->
+        </div><!--end col-->
+    </div>
+    <!-- end page title end breadcrumb -->
+    <div class="row">
+        <div class="col-12 col-lg-8 mx-auto">
+            <div class="card">
+                <div class="card-body">
+                <form method="POST" action="{{ route('categories.store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="name_ar" class="form-label">{{ __('general.attributes.name_ar') }}</label>
+                            <input id="name_ar" type="text" class="form-control @error('name_ar') is-invalid @enderror" name="name_ar" value="{{ old('name_ar') }}" required autocomplete="name_ar" autofocus>
+                            @error('name_ar')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="name_en" class="form-label">{{ __('general.attributes.name_en') }}</label>
+                            <input id="name_en" type="text" class="form-control @error('name_en') is-invalid @enderror" name="name_en" value="{{ old('name_en') }}" required autocomplete="name_en">
+                            @error('name_en')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="image" class="form-label">{{ __('general.attributes.image') }}</label>
+                            <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image">
+                            @error('image')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="parent_id" class="form-label">{{ __('general.attributes.parent_category') }}</label>
+                            <input id="parent_id" type="number" class="form-control @error('parent_id') is-invalid @enderror" name="parent_id" value="{{ old('parent_id') }}">
+                            @error('parent_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="active" class="form-label">{{ __('general.attributes.active') }}</label>
+                            <select id="active" class="form-control @error('active') is-invalid @enderror" name="active" required autocomplete="active">
+                            <option value="1">{{ __('general.select.yes') }}</option>
+                            <option value="0">{{ __('general.select.no') }}</option>
+                            </select>
+                            @error('active')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('general.btn.create') }}
+                            </button>
+                        </div>
+                    </form>
+                </div> <!--end card-body-->                                           
+            </div><!--end card-->
+        </div> <!--end col-->                                          
+    </div><!--end row-->
+
+</div><!-- container -->
+</div>
+
+@endsection
