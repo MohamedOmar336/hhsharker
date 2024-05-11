e46f69c67b6943814552ee9c27e13a4547f3341a
mhameed <mhmedomr336@gmail.com>
2024-05-11 21:00:09 +0300

    footer

--- resources/views/components/app-footer.blade.php
+++ resources/views/components/app-footer.blade.php
@@ -1,65 +1,79 @@
 <footer>
-    <div class="flex flex-col justify-center mt-8 px-16 py-20 bg-blue-950 max-md:px-5">
-        <div class="justify-between mt-8 mr-3 ml-3 max-md:mr-2.5 max-md:max-w-full">
-            <div class="flex gap-5 max-md:flex-col max-md:gap-0">
-                <div class="flex flex-col w-[26%] max-md:ml-0 max-md:w-full">
-                    <div class="flex flex-col grow justify-between pr-3 max-md:mt-10">
-                        <div
-                            class="flex flex-col justify-center items-start px-6 py-4 bg-slate-50 rounded-[64px] max-md:px-5">
-                            <img loading="lazy" src="{{ asset('resources/images/logo.jpg') }}" alt="Company Logo" class="max-w-full aspect-[3.13] w-[101px]" />
-                        </div>
-                        <div class="mt-8 text-sm leading-5 text-white max-md:mt-10">
-                            A leading company in the field of air conditioners and home appliances in Saudi Arabia.
-                        </div>
-                    </div>
+    <div class="container-fluid justify-content-center p-5">
+        <div class="row">
+            {{-- logo --}}
+            <div class="col-md-4 col-12 start">
+                <a href="#">
+                    <img src=" {{ asset('assets-website/images/logos/logo.jpg') }} " alt="Company Logo">
+                </a>
+                <p>
+                    A leading company in the field of air conditioners and home appliances in Saudi Arabia.
+                </p>
+            </div>
+
+            {{-- Links --}}
+            <div class="col-12 col-md-4 d-flex gap-5 middle">
+                <div>
+                    <h5>Company</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Our Products</a>
+                        </li>
+                        <li>
+                            <a href="#">Testimonials</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="flex flex-col ml-5 w-[44%] max-md:ml-0 max-md:w-full">
-                    <div class="max-md:mt-10 max-md:max-w-full">
-                        <div class="flex gap-5 max-md:flex-col max-md:gap-0">
-                            <div class="flex flex-col w-[41%] max-md:ml-0 max-md:w-full">
-                                <div class="flex flex-col grow py-3 text-base font-medium leading-6 text-white max-md:mt-10">
-                                    <div class="text-xl">Company</div>
-                                    <a href="#" class="mt-9">Home</a>
-                                    <a href="#" class="mt-9">About Us</a>
-                                    <a href="#" class="mt-9">Our Products</a>
-                                    <a href="#" class="mt-9">Testimonials</a>
-                                </div>
-                            </div>
-                            <div class="flex flex-col ml-5 w-[38%] max-md:ml-0 max-md:w-full">
-                                <div class="flex flex-col grow py-3 text-base font-medium leading-6 text-white max-md:mt-10">
-                                    <div class="text-xl">Follow Us</div>
-                                    <a href="#" class="mt-9">Instagram</a>
-                                    <a href="#" class="mt-9">YouTube</a>
-                                    <a href="#" class="mt-9">TikTok</a>
-                                    <a href="#" class="mt-9">X</a>
-                                </div>
-                            </div>
-                            <div class="flex flex-col ml-5 w-[21%] max-md:ml-0 max-md:w-full">
-                                <div class="flex flex-col grow py-3 text-base font-medium leading-6 text-white max-md:mt-10">
-                                    <div class="text-xl">Follow Us</div>
-                                    <a href="#" class="mt-9">Instagram</a>
-                                    <a href="#" class="mt-9">YouTube</a>
-                                    <a href="#" class="mt-9">TikTok</a>
-                                    <a href="#" class="mt-9">X</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                <div>
+                    <h5>Follow Us</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="#">YouTube</a>
+                        </li>
+                        <li>
+                            <a href="#">TikTok</a>
+                        </li>
+                        <li>
+                            <a href="#">X</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="flex flex-col ml-5 w-[30%] max-md:ml-0 max-md:w-full">
-                    <div class="flex flex-col grow max-md:mt-10">
-                        <div class="text-4xl text-center text-white uppercase leading-[120px] tracking-[2px] max-md:text-4xl">
-                            Get In touch With H&H Shaker
-                        </div>
-                        <div class="flex mt-5">
-                            <a href="#" class="justify-center flex items-center px-4 py-2 text-base font-medium leading-6 text-center bg-white rounded-[36px] text-blue-950 max-md:px-5">Contact Us</a>
-                            <div class="flex flex-col justify-center items-start p-2 bg-emerald-400 border-2 border-white border-solid rounded-[36px]">
-                                <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/0d75bb0fbc8e5bc2518932fe55b2db0ca4ee6830e9208fdc1c6f572fbf552a47?" alt="Phone Icon" class="w-6 aspect-square" />
-                            </div>
-                        </div>
-                    </div>
+
+                <div>
+                    <h5>Follow Us</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="#">YouTube</a>
+                        </li>
+                        <li>
+                            <a href="#">TikTok</a>
+                        </li>
+                        <li>
+                            <a href="#">X</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
+
+            {{-- Contact --}}
+            <div class="col-md-4 col-12 end">
+                <h1>Get In touch With H&H Shaker</h1>
+                <x-arrow-button>Contact Us</x-arrow-button>
+            </div>
+        </div>
         </div>
     </div>
 </footer>
