6ff1349cafbfebd69858bda95d55ca068d2f4545
mhameed <mhmedomr336@gmail.com>
2024-05-13 21:56:11 +0300

    section 2 - brands

--- resources/views/website/home/_2.blade.php
+++ resources/views/website/home/_2.blade.php
@@ -1,4 +1,4 @@
-<section class="container-lg">
+<section id="brands" class="container-lg py-5">
     <div class="row">
         <div class="col-6">
             <x-tag class="figure">Quick Access</x-tag>
@@ -7,16 +7,12 @@
                 OUR TRUSTED BRANDS
             </h2>
         </div>
-        <div class="col-6 col-6 d-flex justify-content-between">
-            <div class="d-flex flex-column w-50">
-                <div class="d-flex justify-content-center align-items-center px-4 py-3 bg-light border border-white rounded-3">
-                    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/ae2e39163c1f6a366a3227d60c1668bfec50c20b582b54603bb583d4bf66da0e?apiKey=95e93e0986c543eeab0cc2ed468bab82&" alt="Brand logo" class="w-100 h-auto" />
-                </div>
+        <div class="brands-container col-6 col-6 d-flex justify-content-between gap-3">
+            <div class="align-items-center border d-flex justify-content-center px-4 py-3 rounded-5">
+                <img loading="lazy" src="{{ asset('assets-website/images/logos/midea.png') }}" alt="midea">
             </div>
-            <div class="d-flex flex-column w-50">
-                <div class="d-flex justify-content-center align-items-center px-4 py-3 bg-light border border-white rounded-3">
-                    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/4352e08cdef7030ea176191d6609e4005bef5ea967cc8facd9c25482d30e231d?apiKey=95e93e0986c543eeab0cc2ed468bab82&" alt="Brand logo" class="w-100 h-auto" />
-                </div>
+            <div class="align-items-center border d-flex justify-content-center px-4 py-3 rounded-5">
+                <img loading="lazy" src="{{ asset('assets-website/images/logos/beko.png') }}" alt="beko">
             </div>
         </div>
     </div>
