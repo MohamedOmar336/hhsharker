1c269d91108f1c6950d3ea67e5ceb6cd2493d9a0
mhameed <mhameed@ntgclarity.com>
2024-05-24 15:23:05 +0300

    add datatable in user

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -15,79 +15,67 @@
                                     <li class="breadcrumb-item active">{{ __('general.attributes.users') }}</li>
                                 </ol>
                             </div>
-                             <div class="col-md-12">
-                    <a href="{{ URL::previous() }}"
-                    class="btn btn-secondary"><span class="fa fa-backward"></a>
-                     <h4 class="page-title">{{ __('general.attributes.users').' ' }} List</h4>
-                </div>
-                          
+                            <div class="col-md-12">
+                                <a href="{{ URL::previous() }}" class="btn btn-secondary"><span class="fa fa-backward"></a>
+                                <h4 class="page-title">{{ __('general.attributes.users') . ' ' }} List</h4>
+                            </div>
+
                         </div><!--end page-title-box-->
                     </div><!--end col-->
                 </div>
                 <!-- end page title end breadcrumb -->
 
-                <div class="row">
-                    <div class="col-12">
-                       <div class="card">
-                         <div class="card-body content-area">
-                           <div class="table-responsive browser_users">
-                                        <table class="table mb-0">
-                                      
-						  <thead class="thead-light">
-                                           <tr>
-                                                <th scope="col">{{ __('general.attributes.users') }}</th>
-                                                <th scope="col">{{ __('general.side.roles') }}</th>
-                                                <th scope="col">{{ __('general.attributes.email') }}</th>
-                                                <th scope="col">{{ __('general.attributes.phone') }}</th>
-                                                <th scope="col">{{ __('general.attributes.status') }}</th>
-                                                <th scope="col">{{ __('general.attributes.actions') }}</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($records as $record)
-                                                <tr class="spacer">
-                                                    <td><img src="{{ asset('images/' . $record->image) }}"
-                                                            alt="" class="rounded-circle thumb-sm me-1">
-                                                        {{ $record->user_name }}
-                                                    </td>
-                                                    <td>Administrator</td>
-                                                    <td>{{ $record->email }}</td>
-                                                    <td>{{ $record->phone }}</td>
-                                                    @if ($record->active)
-                                                        <td><span class="badge badge-soft-success">Active</span></td>
-                                                    @else
-                                                        <td><span class="badge badge-soft-danger">Deactivated</span></td>
-                                                    @endif
-                                                    <td>
-                                                        <a href="{{ route('users.edit', $record->id) }}"
-                                                            class="btn btn-sm btn-primary">Edit</a>
-                                                        <form action="{{ route('users.destroy', $record->id) }}"
-                                                            method="POST" style="display: inline;">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit" class="btn btn-sm btn-danger"
-                                                                onclick="return confirm('Are you sure you want to delete this user?')">{{ __('general.btn.delete') }}</button>
-                                                        </form>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
 
-                                        </tbody>
-                                    </table><!--end /table-->
-                                </div><!--end /tableresponsive-->
-                                <div class="row">
-                                    <div class="col">
-                                        <a class="btn btn-outline-light btn-sm px-4"
-                                            href="{{ route('users.create') }}">{{ __('general.actions.new') }}</a>
-                                    </div><!--end col-->
-                                    <div class="col-auto">
-                                        {{ $records->links('admin.pagination.bootstrap') }}
-                                    </div> <!--end col-->
-                                </div><!--end row-->
-                            </div><!--end card-body-->
-                        </div><!--end card-->
-                    </div> <!-- end col -->
-                </div><!--end row-->
+                <x-table tableId="DataTables">
+                    <x-slot name="header">
+                        <tr>
+                            <th><input type="checkbox" id="select-all"></th>
+                            <th scope="col">{{ __('general.attributes.users') }}</th>
+                            <th scope="col">{{ __('general.side.roles') }}</th>
+                            <th scope="col">{{ __('general.attributes.email') }}</th>
+                            <th scope="col">{{ __('general.attributes.phone') }}</th>
+                            <th scope="col">{{ __('general.attributes.status') }}</th>
+                            <th scope="col">{{ __('general.attributes.actions') }}</th>
+                        </tr>
+                    </x-slot>
+
+                    @foreach ($records as $record)
+                        <tr>
+                            <td><input type="checkbox" name="ids[]" value="{{ $record->id }}"></td>
+                            <td><img src="{{ asset('images/' . $record->image) }}" alt=""
+                                    class="rounded-circle thumb-sm me-1">
+                                {{ $record->user_name }}
+                            </td>
+                            <td>Administrator</td>
+                            <td>{{ $record->email }}</td>
+                            <td>{{ $record->phone }}</td>
+                            @if ($record->active)
+                                <td><span class="badge badge-soft-success">Active</span></td>
+                            @else
+                                <td><span class="badge badge-soft-danger">Deactivated</span></td>
+                            @endif
+                            <td>
+                                <a href="{{ route('users.edit', $record->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                <form action="{{ route('users.destroy', $record->id) }}" method="POST"
+                                    style="display: inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger"
+                                        onclick="return confirm('Are you sure you want to delete this user?')">{{ __('general.btn.delete') }}</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    <x-slot name="createButton">
+                        <a href="{{ route('products.create') }}" class="btn btn-outline-light btn-sm px-4">+
+                            {{ __('general.actions.new') }}</a>
+                    </x-slot>
+
+                    <x-slot name="pagination">
+                        {{ $records->links('admin.pagination.bootstrap') }}
+                    </x-slot>
+                </x-table>
 
             </div><!-- container -->
         </div>
