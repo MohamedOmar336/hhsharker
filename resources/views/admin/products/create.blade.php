40ef342d142bb5dd92a4212cf4f7276e4372a6f5
Batool Anwar <hardanbatool@gmail.com>
2024-06-22 14:46:00 +0200

    Your commit message

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -188,10 +188,6 @@
                                     @enderror
                                 </div>
 
-<<<<<<< HEAD
-                                <div class="form-group mb-0">
-                                    <x-btn name="{{ __('general.btn.submit') }}"></x-btn>
-=======
                                 <div class="mb-3">
                                     <label for="category_id" class="form-label">{{ __('general.attributes.category') }}</label>
                                     <select id="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id" required>
@@ -209,7 +205,6 @@
 
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-primary">{{ __('general.btn.create') }}</button>
->>>>>>> 4e0b96d8b33558e50b67fb5775d711594a70797f
                                 </div>
                             </form>
                         </div> <!--end card-body-->
