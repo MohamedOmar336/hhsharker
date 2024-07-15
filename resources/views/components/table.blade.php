4f971f80b3949671d78d0267f752c139079d1f2a
mhameed <mhameed@ntgclarity.com>
2024-07-15 20:00:13 +0300

    fix btns in index ( add btn - delete btn )

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,14 +1,15 @@
 <div class="card">
     <div class="card-body content-area">
-      
+
         <form id="bulk-delete-form" method="get" action="{{ isset($createButton->attributes['action'] ) ?  $createButton->attributes['action'] : null }}">
             @csrf
             @if(isset($createButton))
-            <div class="col col-3">
-                <button class="btn btn-outline-light btn-sm px-4" id="bulk-delete-btn">{{ __('general.bulk-delete') }}</button>
-                {{ $createButton }}
-            </div>
-        @endif
+                <div id="table-custom-actions">
+                    <button class="btn btn-outline-light btn-sm px-4" id="bulk-delete-btn">{{ __('general.bulk-delete') }}</button>
+                    {{ $createButton }}
+                </div>
+            @endif
+            
             <div class="table-responsive">
                 <table class="table mb-0" id="{{ $tableId }}">
                     <thead class="thead-light">
