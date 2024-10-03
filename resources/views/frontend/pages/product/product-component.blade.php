162936bc263cea763b8b7d0e5184b91830fcc2dc
mhameed <mhameed@ntgclarity.com>
2024-10-03 10:41:18 +0300

    add default image to products

--- resources/views/frontend/pages/product/product-component.blade.php
+++ resources/views/frontend/pages/product/product-component.blade.php
@@ -1,13 +1,14 @@
 
 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
     <div class="inner-list-div">
-        <img src="{{ Storage::url($product->product_image) }}">
+        <img src="{{ isset($product->product_image) ?  Storage::url($product->product_image) : asset('assets-frontend/images/inner-prod-1.png')}}">
+
         <div class="innter-body-text">
             <h3>{{ $product->name ?? ''}}</h3>
             @if(isset($product->children[0]->model_number))
                 <p>{{ $product->children[0]->model_number }}</p>
             @endif
-            
+
             @if(!empty($product->features))
                 <div>
                     @foreach (array_slice($product->features,0,4) as $feature)
